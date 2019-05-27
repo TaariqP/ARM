3d52cdabd4301eb55061397888e352b48262320f
Satadru Sanyal <ss13718@ray08.doc.ic.ac.uk>
2019-05-27 19:53:44 +0100

    added file for instruction assembly functions, working on branch assembly

--- src/assemble_utils/utils.c
+++ src/assemble_utils/utils.c
@@ -19,9 +19,23 @@ void binary_file_writer(char *filename, const char *binary_string){
     fwrite(filename, 1, sizeof(binary_string),binary_file);
     fclose(binary_file);
 }
-    
 
+void extract_branch_cond(char *string, char result[3]){
+    if (string[1] != ' ') {
+        result[0] = string[1];
+        result[1] = string[2];
+        result[2] = '/0';
+    } else {
+        result[0] = ' ';
+        result[1] = ' ';
+        result[2] = '/0';
+    }
+}
 
+uint32_t set_4_bits(uint32_t binary, int end_bit, int value){
+    binary |= (value << end_bit);
+    return binary;
+}
 
 void two_pass_assembly(){
 
