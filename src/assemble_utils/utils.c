b96255ea0dbacf00cb2f0945ff0c21988320db03
TaariqP <taariq_pala@hotmail.co.uk>
2019-05-28 16:41:17 +0100

    Working on second pass

--- src/assemble_utils/utils.c
+++ src/assemble_utils/utils.c
@@ -16,7 +16,7 @@ void binary_file_writer(char *filename, const char *binary_string) {
     fclose(binary_file);
 }
 
-void extract_2_char_cond(char *string, char result[3]){
+void extract_2_char_cond(char *string, char result[3]) {
     if (string[1] != ' ') {
         result[0] = string[1];
         result[1] = string[2];
@@ -33,7 +33,7 @@ void extract_2_char_cond(char *string, char result[3]){
 
  assumption that bits being set start as 0, as do all bits less significant
  than end bit. */
-uint32_t set_n_bits(uint32_t binary, int end_bit, int value){
+uint32_t set_n_bits(uint32_t binary, int end_bit, int value) {
     binary |= (value << end_bit);
     return binary;
 }
@@ -52,12 +52,10 @@ char *two_pass_assembly(char **code, int line_num) {
     };
 
     //First pass assoociates labels with memory addresses.
-    first_pass(code, line_num, tokenised_line, symbol_table);
+    first_pass(code, tokenised_line, symbol_table);
 
     /* Second Pass */
-    char* binary = (char *) malloc(INSTRUCTION_SIZE * LINES);
-    binary[0] = '\0';
-
+    char *binary = second_pass(code, tokenised_line, symbol_table);
 
     // REMEMBER TO free variables
 
@@ -65,29 +63,46 @@ char *two_pass_assembly(char **code, int line_num) {
 
 }
 
-void add_to_mappings(symbol_table* symbol_table, mapping mapping){
+void add_to_mappings(symbol_table *symbol_table, mapping mapping) {
     int num_elements = symbol_table->num_elements;
     symbol_table->num_elements = symbol_table->num_elements + 1;
     symbol_table->mappings[num_elements] = mapping;
 }
 
-void first_pass(char **code, int line_num, tokenised_line tokenised_line, symbol_table symbol_table) {
+void first_pass(char **code, tokenised_line tokenised_line, symbol_table symbol_table) {
     char *line;
-    for (int i = 0; i < LINE_LENGTH; ++i) {
+    for (int line_num = 0; line_num < LINES; line_num++) {
         line = code[line_num];
         int operand_num = tokenizer(line, tokenised_line);
-        if (operand_num == 0){
+        if (operand_num == 0) {
             printf("label: %s\n", tokenised_line.label);
             mapping mapping = {
                     .label = tokenised_line.label,
-                    .memory_address = &code[line_num+1]
+                    .memory_address = &code[line_num + 1]
             };
             add_to_mappings(&symbol_table, mapping);
         }
     }
 }
 
-char* second_pass(){
+char *second_pass(char **code, tokenised_line tokenised_line, symbol_table symbol_table) {
+
+    char *binary = (char *) malloc(INSTRUCTION_SIZE * LINES);
+    binary[0] = '\0';
+    //Read opcode mnemonics + operands for each instruction
+    for (int i = 0; i < LINES; ++i) {
+        int num_of_operands = tokenizer(code[i], tokenised_line);
+
+        //exclude labels
+        if (num_of_operands != 0) {
+            for (int j = 0; j < num_of_operands; ++j) {
+                uintptr_t address;
+
+            }
+        }
+    }
+
+    return binary;
 
 }
 
