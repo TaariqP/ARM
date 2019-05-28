dced70f2198bc768e418bd63f8e4fd5f91f1089f
Satadru Sanyal <ss13718@sprite26.doc.ic.ac.uk>
2019-05-28 15:34:41 +0100

    made changes to allow tests to run, also added #include statements for compilation, need to fix later

--- src/emulate.c
+++ src/emulate.c
@@ -6,6 +6,9 @@
 #include "emulate_utils/execute.h"
 #include "emulate_utils/decode.h"
 #include "emulate_utils/defs.h"
+#include "emulate_utils/utils.c"
+#include "emulate_utils/execute.c"
+#include "emulate_utils/decode.c"
 
 
 void execute(current_state *state) {
@@ -96,7 +99,7 @@ void pipeline_cycle(current_state *state, int size) {
 
 }
 
-/*
+
 int main(int argc, char **argv) {
 
     if (argc != 2) {
@@ -134,5 +137,3 @@ int main(int argc, char **argv) {
 
     return EXIT_SUCCESS;
 }
-
-*/
