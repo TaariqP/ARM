e9309f39982c9279641e58149a5fc308481866bf
Satadru Sanyal <ss13718@ic.ac.uk>
2019-05-28 02:05:35 +0100

    worked further on instruction assembler template, worked out logic for branch which includes TODOs for future reference. Adjusted header files accordingly

--- src/assemble_utils/instruction_assembler.h
+++ src/assemble_utils/instruction_assembler.h
@@ -5,12 +5,12 @@
 #ifndef ARM11_11_INSTRUCTION_ASSEMBLER_H
 #define ARM11_11_INSTRUCTION_ASSEMBLER_H
 
-uint32_t assemble_dpi(char*, uint32_t*);
+uint32_t assemble_dpi(char*, uint32_t*, char **, int, symbol_table *);
 
-uint32_t assemble_sdt(char*, uint32_t*);
+uint32_t assemble_sdt(char*, uint32_t*, char **, int, symbol_table *);
 
-uint32_t assemble_mul(char*, uint32_t*);
+uint32_t assemble_mul(char*, uint32_t*, char **, int, symbol_table *);
 
-uint32_t assemble_branch(char*, uint32_t*);
+uint32_t assemble_branch(char*, uint32_t*, char **, int, symbol_table *);
 
 #endif //ARM11_11_INSTRUCTION_ASSEMBLER_H
