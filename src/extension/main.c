d8ee6cba5aeebb5fa972499d884c28c933d34dac
Satadru Sanyal <ss13718@sprite03.doc.ic.ac.uk>
2019-06-12 20:33:46 +0100

    using ncurses for extension, makefile works

--- src/extension/main.c
+++ src/extension/main.c
@@ -2,4 +2,15 @@
 // Created by taariq on 6/11/19.
 //
 
+#include <ncurses.h>
+#include <unistd.h>
 
+int main(void){
+    initscr(); // Initialize the window
+    noecho(); // Don't echo any keypresses
+    curs_set(FALSE); // Don't display a cursor
+
+    sleep(2);
+
+    endwin();
+}
